931ea4173e2391deec57677e28de61f06d8d6ac6
TNTSuperMan <tnt-super-man@outlook.com>
2025-07-30 01:58:39 +0900

    利便性の都合から、blocksは純粋なブロックリストにし、block.jsonの出力はbuild.tsに統合

--- build.ts
+++ build.ts
@@ -1,4 +1,17 @@
 import { file, write } from "bun";
-import"./src/blocks";
+import { blacklist } from "./src/blocks";
+
+const json = blacklist.map((e,i)=>({
+    id: i+1,
+    priority: 1,
+    action: { type: "block" },
+    condition: {
+        regexFilter: e instanceof RegExp ? e.source : undefined,
+        urlFilter: typeof e === "string" ? e : undefined,
+        resourceTypes: [ "main_frame", "sub_frame", "stylesheet", "script", "image", "font", "object", "xmlhttprequest", "ping", "csp_report", "media", "websocket", "webtransport", "webbundle", "other" ]
+    }
+}))
+
+write("./dist/block.json", JSON.stringify(json));
 
 write("./dist/manifest.json", file("./manifest.json"));
--- src/blocks.ts
+++ src/blocks.ts
@@ -1,18 +1,3 @@
-import { write } from "bun";
-
-const blacklist = [
+export const blacklist = [
     /^https?:\/\/(\w*\.)*(google|google-analytics|googletagmanager|youtube|googlesyndication|doubleclick|gstatic|googleapis|googleusercontent|googleemail|googlecode|googlehosted|googledrive|googleearth|googlefonts)(\.\w{1,3}){1,2}\//,
 ] as RegExp[];
-
-const json = blacklist.map((e,i)=>({
-    id: i+1,
-    priority: 1,
-    action: { type: "block" },
-    condition: {
-        regexFilter: e instanceof RegExp ? e.source : undefined,
-        urlFilter: typeof e === "string" ? e : undefined,
-        resourceTypes: [ "main_frame", "sub_frame", "stylesheet", "script", "image", "font", "object", "xmlhttprequest", "ping", "csp_report", "media", "websocket", "webtransport", "webbundle", "other" ]
-    }
-}))
-
-write("./dist/block.json", JSON.stringify(json));
