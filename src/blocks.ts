861a0933ea57550f5f42c39c0f481139b4ffa850
TNTSuperMan <tnt-super-man@outlook.com>
2025-07-30 01:57:37 +0900

    型を(RegExp|string)[]からRegExp[]にする

--- src/blocks.ts
+++ src/blocks.ts
@@ -2,7 +2,7 @@ import { write } from "bun";
 
 const blacklist = [
     /^https?:\/\/(\w*\.)*(google|google-analytics|googletagmanager|youtube|googlesyndication|doubleclick|gstatic|googleapis|googleusercontent|googleemail|googlecode|googlehosted|googledrive|googleearth|googlefonts)(\.\w{1,3}){1,2}\//,
-] as (RegExp | string)[];
+] as RegExp[];
 
 const json = blacklist.map((e,i)=>({
     id: i+1,
