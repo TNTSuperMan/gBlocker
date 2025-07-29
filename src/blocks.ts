5f079b8f962b53d3ae0b6f84670083512475b6f5
TNTSuperMan <tnt-super-man@outlook.com>
2025-07-30 01:56:35 +0900

    わあもう全部一つに入れちゃえー(白目)

--- src/blocks.ts
+++ src/blocks.ts
@@ -1,21 +1,7 @@
 import { write } from "bun";
 
 const blacklist = [
-    /^https?:\/\/(\w*\.)*(google)(\.\w{1,3}){1,2}\//,
-    "||google-analytics.com",
-    "||googletagmanager.com",
-    "||youtube.com",
-    "||googlesyndication.com",
-    "||doubleclick.net",
-    "||gstatic.com",
-    "||googleapis.com",
-    "||googleusercontent.com",
-    "||googleemail.com",
-    "||googlecode.com",
-    "||googlehosted.com",
-    "||googledrive.com",
-    "||googleearth.com",
-    "||googlefonts.com",
+    /^https?:\/\/(\w*\.)*(google|google-analytics|googletagmanager|youtube|googlesyndication|doubleclick|gstatic|googleapis|googleusercontent|googleemail|googlecode|googlehosted|googledrive|googleearth|googlefonts)(\.\w{1,3}){1,2}\//,
 ] as (RegExp | string)[];
 
 const json = blacklist.map((e,i)=>({
